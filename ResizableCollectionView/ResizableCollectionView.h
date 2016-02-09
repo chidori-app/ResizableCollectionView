6a7ff9dfa852500bfd0df68aaae83d3add9ce3ff
ioka <hit0apps@gmail.com>
2016-02-10 08:17:53 +0900

    separate sample from library

---
+++ ResizableCollectionView/ResizableCollectionView.h
@@ -0,0 +1,19 @@
+//
+//  ResizableCollectionView.h
+//  ResizableCollectionView
+//
+//  Created by hitting on 2016/02/10.
+//  Copyright © 2016年 nscallop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ResizableCollectionView.
+FOUNDATION_EXPORT double ResizableCollectionViewVersionNumber;
+
+//! Project version string for ResizableCollectionView.
+FOUNDATION_EXPORT const unsigned char ResizableCollectionViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ResizableCollectionView/PublicHeader.h>
+
+
